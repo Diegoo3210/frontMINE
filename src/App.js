7d02f782a6aaf5d04cdc1d86c2bdf5055726e952
Diegoo3210 <alejito3210@icloud.com>
2023-04-13 02:28:31 -0500

    Head fixed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './pages/HomePage';
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Taller1 from './pages/Taller1';
-
-
+import Taller1 from "./pages/Taller1";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Analis de Información"; // Change the title here
+  }, []);
+
   return (
     <div>
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/Tarea1" element={<Taller1/>}/>
-      <Route path="/Taller1" element={<Taller1/>}/>
-      </Routes>
-    </BrowserRouter>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Tarea1" element={<Taller1 />} />
+          <Route path="/Taller1" element={<Taller1 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
